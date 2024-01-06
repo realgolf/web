f1610069ba1288ba31c4988e0b211390626bb829
Julian Hammer <jh@moinjulian.com>
2024-01-06 13:05:59 +0100

    renamed modi to games

--- src/routes/dashboard/games/+page.svelte
+++ src/routes/dashboard/games/+page.svelte
@@ -3,19 +3,19 @@
 
   let fourwinning = [
     {
-      link: "/dashboard/modi/4Winning/Two-Players",
+      link: "/dashboard/games/4Winning/Two-Players",
       title: "Two Players",
       description:
         "This version of 4Winning can be played with Two Players,  Red and  Blue",
     },
     {
-      link: "/dashboard/modi/4Winning/Three-Players",
+      link: "/dashboard/games/4Winning/Three-Players",
       title: "Three Players",
       description:
         "This version of 4Winning can be played with Three Players, Red, Blue and Green",
     },
     {
-      link: "/dashboard/modi/4Winning/Four-Players",
+      link: "/dashboard/games/4Winning/Four-Players",
       title: "Four Players",
       description:
         "This version of 4Winning can be played with Four Players, Red, Blue, Green and Orange",
@@ -24,19 +24,19 @@
 
   let exact = [
     {
-      link: "/dashboard/modi/Exact/Two-Players",
+      link: "/dashboard/games/Exact/Two-Players",
       title: "Two Players",
       description:
         "This version of Exact can be played with Two Players,  Red and  Blue",
     },
     {
-      link: "/dashboard/modi/Exact/Three-Players",
+      link: "/dashboard/games/Exact/Three-Players",
       title: "Three Players",
       description:
         "This version of Exact can be played with Three Players, Red, Blue and Green",
     },
     {
-      link: "/dashboard/modi/Exact/Four-Players",
+      link: "/dashboard/games/Exact/Four-Players",
       title: "Four Players",
       description:
         "This version of Exact can be played with Four Players, Red, Blue, Green and Orange",
@@ -45,25 +45,25 @@
 
   let precision = [
     {
-      link: "/dashboard/modi/Precision/One-Player",
+      link: "/dashboard/games/Precision/One-Player",
       title: "One Player",
       description:
         "This version of Precision can be played with One Player, Red",
     },
     {
-      link: "/dashboard/modi/Precision/Two-Players",
+      link: "/dashboard/games/Precision/Two-Players",
       title: "Two Players",
       description:
         "This version of Precision can be played with Two Players, Red, Blue ",
     },
     {
-      link: "/dashboard/modi/Precision/Three-Players",
+      link: "/dashboard/games/Precision/Three-Players",
       title: "Three Players",
       description:
         "This version of Precision can be played with Three Players, Red, Blue and Green",
     },
     {
-      link: "/dashboard/modi/Precision/Four-Players",
+      link: "/dashboard/games/Precision/Four-Players",
       title: "Four Players",
       description:
         "This version of Precision can be played with Four Players, Red, Blue, Green and Orange",
@@ -72,14 +72,14 @@
 </script>
 
 <svelte:head>
-  <title>Real Golf - Modi</title>
+  <title>Real Golf - Games</title>
 </svelte:head>
 
-<h1>Modi</h1>
+<h1>Games</h1>
 
 <main>
   <p class="success">
-    Here you can find the different modi you can play the games in.
+    Here you can find the different modi you can play the Games in.
   </p>
 
   <details>
@@ -91,10 +91,10 @@
     </ul>
   </details>
 
-  <div class="modi">
+  <div class="games">
     <h2 id="4-winning">4 Winning</h2>
 
-    <div class="modis">
+    <div class="gamess">
       {#each fourwinning as { link, title, description }}
         <Card {link} {title} {description} />
       {/each}
@@ -102,7 +102,7 @@
 
     <h2 id="exact">Exact</h2>
 
-    <div class="modis">
+    <div class="gamess">
       {#each exact as { link, title, description }}
         <Card {link} {title} {description} />
       {/each}
@@ -110,7 +110,7 @@
 
     <h2 id="precision">Precision</h2>
 
-    <div class="modis">
+    <div class="gamess">
       {#each precision as { link, title, description }}
         <Card {link} {title} {description} />
       {/each}
@@ -119,14 +119,14 @@
 </main>
 
 <style lang="scss">
-  .modi {
+  .games {
     display: flex;
     flex-direction: row;
     flex-wrap: wrap;
     width: 90vw;
   }
 
-  .modis {
+  .gamess {
     width: 100%;
     display: flex;
     flex-direction: row;
