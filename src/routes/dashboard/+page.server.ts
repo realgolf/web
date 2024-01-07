a46e335542dd97c4a9f6daf1c103b3ff91db70e6
Julian Hammer <jh@moinjulian.com>
2024-01-07 14:16:38 +0100

    Update dashboard page to display One Player Precision highscores.

---
+++ src/routes/dashboard/+page.server.ts
@@ -0,0 +1,42 @@
+import { User_Model } from "$lib/server/models";
+
+export async function load(event): Promise<unknown> {
+  const email = event.cookies.get("email");
+
+  const user = await User_Model.findOne({ "user.email": email });
+
+  if (!user) {
+    return { status: 400, error: "User could not be found" };
+  } else {
+    const one_player_precision_highscore = user?.one_player_precision_highscore;
+
+    const daily = {
+      value: one_player_precision_highscore?.daily?.value,
+      lastUpdated: one_player_precision_highscore?.daily?.lastUpdated,
+    };
+
+    const weekly = {
+      value: one_player_precision_highscore?.weekly?.value,
+      lastUpdated: one_player_precision_highscore?.weekly?.lastUpdated,
+    };
+
+    const monthly = {
+      value: one_player_precision_highscore?.monthly?.value,
+      lastUpdated: one_player_precision_highscore?.monthly?.lastUpdated,
+    };
+
+    const yearly = {
+      value: one_player_precision_highscore?.yearly?.value,
+      lastUpdated: one_player_precision_highscore?.yearly?.lastUpdated,
+    };
+
+    const all_time = {
+      value: one_player_precision_highscore?.all_time?.value,
+      lastUpdated: one_player_precision_highscore?.all_time?.lastUpdated,
+    };
+
+    console.log(daily, weekly, monthly, yearly, all_time);
+
+    return { daily, weekly, monthly, yearly, all_time };
+  }
+}
--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,7 +1,5 @@
 <script lang="ts">
-  import type { PageData } from "./$types";
-
-  export let data: PageData;
+  export let data: any;
 </script>
 
 <svelte:head>
@@ -19,7 +17,56 @@
     </b>
   </p>
 
-<p class="success">The navigation can be found in the dropdown menu.</p>
+  <p class="success">The navigation can be found in the dropdown menu.</p>
 
   <h3>But most important enjoy the games and have fun!</h3>
+
+  <div class="card">
+    <h2>One Player Precision Highscore</h2>
+    {#if data.daily.lastUpdated == null}
+      <p>
+        Please first play a round of <a
+          href="/dashboard/games/Precision/One-Player">One Player Precision</a
+        > to get a Highscore before it can be displayed here.
+      </p>
+    {:else}
+      <ul>
+        <li>
+          <b>Today's</b> highscore is: {data.daily.value}, last updated on{" "}
+          {new Date(data.daily.lastUpdated).toLocaleDateString()}.
+        </li>
+        <li>
+          <b>This Week's</b> highscore is: {data.weekly.value}, last updated on{" "}
+          {new Date(data.weekly.lastUpdated).toLocaleDateString()}.
+        </li>
+        <li>
+          <b>This Month's</b> highscore is: {data.monthly.value}, last updated
+          on{" "}
+          {new Date(data.monthly.lastUpdated).toLocaleDateString()}.
+        </li>
+        <li>
+          <b>This Year's</b> highscore is: {data.yearly.value}, last updated on{" "}
+          {new Date(data.yearly.lastUpdated).toLocaleDateString()}.
+        </li>
+      </ul>
+      <p>
+        Your all-time highscore is <b>{data.all_time.value}</b>, last exceeded
+        on{" "}
+        {new Date(data.all_time.lastUpdated).toLocaleDateString()}.
+      </p>
+    {/if}
+  </div>
 </main>
+
+<style lang="scss">
+  .card {
+    border: 2px solid var(--border-color);
+    margin: 10px 20px;
+    padding: 15px;
+    box-shadow: 2px 2px 4px var(--shadow-color);
+
+    ul {
+      margin-left: 1rem;
+    }
+  }
+</style>
