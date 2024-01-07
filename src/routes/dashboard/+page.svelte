764f017a58e6b798506d84eec7d0e82673683661
Julian Hammer <jh@moinjulian.com>
2024-01-07 13:11:49 +0100

    Update navigation instructions and remove navigation cards.

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-  import Card from "$lib/components/Card.svelte";
   import type { PageData } from "./$types";
 
   export let data: PageData;
@@ -20,60 +19,9 @@
     </b>
   </p>
 
-  <p class="success">
-    You can find all the games in the <b>Modi</b> Card below.
-  </p>
-  <p class="success">
-    Everything about the Rules is in the Card labeld <b>Rules</b>.
-  </p>
-  <p class="success">
-    If the normal games are to boring, you can find more challenging ideas in
-    the Card labeled <b>Levels</b>.
-  </p>
+<p class="success">The navigation can be found in the dropdown menu.</p>
 
   <h3>But most important enjoy the games and have fun!</h3>
-
-  <div class="modi">
-    <div class="modis">
-      <Card
-        link="/dashboard/rules"
-        title="Rules"
-        description="Here you can see all rules for all three games."
-      />
-
-      <Card
-        link="/dashboard/games"
-        title="Games"
-        description="Here you can find all game modes you can play, just click on the link for
-the game you want to play."
-      />
-
-      <Card
-        link="/dashboard/levels"
-        title="Levels"
-        description="Some extra variation, if the games are to easy for you."
-      />
-    </div>
-    <div class="modis">
-      <Card
-        link="/dashboard/archive"
-        title="Archive"
-        description="Here you can find all games you have saved in the database."
-      />
-
-      <Card
-        link="/dashboard/info"
-        title="Info"
-        description="Just some random further information."
-      />
-
-      <Card
-        link="/dashboard/settings"
-        title="Settings"
-        description="Here you can edit your Account settings"
-      />
-    </div>
-  </div>
 </main>
 
 <style lang="scss">
