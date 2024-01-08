17da02279d087a811ac6f8e0d0e1aae37c62a2b3
Julian Hammer <jh@moinjulian.com>
2024-01-08 15:02:51 +0000

    Update cookie maxAge from one day to one week.

--- src/lib/server/utils.ts
+++ src/lib/server/utils.ts
@@ -1,11 +1,11 @@
 export const email_regexp = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/;
 
-export const one_day = 60 * 60 * 24;
+export const one_week = 7 * 60 * 60 * 24;
 
 export const cookie_options = {
   httpOnly: true,
   secure: true,
   sameSite: "strict",
   path: "/",
-  maxAge: one_day,
+  maxAge: one_week,
 } as const;
