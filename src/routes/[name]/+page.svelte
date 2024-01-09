70623e82450a9ad607259ad838dea677ffe8baa3
Julian Hammer <jh@moinjulian.com>
2024-01-09 19:06:35 +0000

    Add One Player Precision highscore section to user profile page

--- src/routes/[name]/+page.svelte
+++ src/routes/[name]/+page.svelte
@@ -43,6 +43,40 @@
   <a href={`/${data.user_username}/games`}>here</a>.
 </p>
 
+<div id="one_player_precision">
+  <h2>One Player Precision Highscore</h2>
+  {#if data.user_daily?.lastUpdated === null}
+    <p>{data.user_username} has never played One Player Precision</p>
+  {:else}
+    <ul>
+      <li>
+        <b>Today's</b> highscore is: {data.user_daily?.value}, last updated on{" "}
+        {new Date(data.user_daily?.lastUpdated).toLocaleDateString()}.
+      </li>
+      <li>
+        <b>This Week's</b> highscore is: {data.user_weekly?.value}, last updated
+        on{" "}
+        {new Date(data.user_weekly?.lastUpdated).toLocaleDateString()}.
+      </li>
+      <li>
+        <b>This Month's</b> highscore is: {data.user_monthly?.value}, last
+        updated on{" "}
+        {new Date(data.user_monthly?.lastUpdated).toLocaleDateString()}.
+      </li>
+      <li>
+        <b>This Year's</b> highscore is: {data.user_yearly?.value}, last updated
+        on{" "}
+        {new Date(data.user_yearly?.lastUpdated).toLocaleDateString()}.
+      </li>
+    </ul>
+    <p>
+      {data.user_name}'s' all-time highscore is
+      <b>{data.user_all_time?.value}</b>, last exceeded on{" "}
+      {new Date(data.user_all_time?.lastUpdated).toLocaleDateString()}.
+    </p>
+  {/if}
+</div>
+
 <style lang="scss">
   .vcard-names {
     margin: 3rem;
@@ -57,6 +91,22 @@
     .vcard-nickname {
       line-height: 24px;
       color: grey !important;
+    }
+  }
+
+  #one_player_precision {
+    background-color: var(--nav-color);
+    margin-left: 20px;
+    border: 1px solid var(--border-color);
+    border-radius: 5px;
+
+    & > * {
+      padding: 20px;
+    }
+
+    ul {
+      margin-left: 20px;
+      list-style-type: square;
     }
   }
 </style>
