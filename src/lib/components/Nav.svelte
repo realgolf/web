75338928df19e7d806a33d7332e1b98965277069
Julian Hammer <jh@moinjulian.com>
2024-01-11 13:44:58 +0000

    Update the navigation bar to display the current page as bold and capitalize the first letter of each page name.

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -26,28 +26,9 @@
 
   function getPagePath() {
     const path = currentPagePath;
+
     if (path === "/") {
       return "<strong>Home</strong>";
-    }
-
-    if (path === "/dashboard") {
-      return "<strong>Dashboard</strong>";
-    }
-
-    if (path === "/login") {
-      return "<strong>Login</strong>";
-    }
-
-    if (path === "/register") {
-      return "<strong>Register</strong>";
-    }
-
-    if (path === "/faq") {
-      return "<strong>FAQ</strong>";
-    }
-
-    if (path === "/imprint") {
-      return "<strong>Imprint</strong>";
     }
 
     const pathParts = path.split("/").filter((part) => part !== "");
@@ -56,27 +37,20 @@
     for (let i = 0; i < pathParts.length; i++) {
       const pathBefore = pathParts.slice(0, i + 1).join("/");
       const isLastSegment = i === pathParts.length - 1;
+      const isFirstSegment = i === 0;
 
       if (isLastSegment) {
-        breadcrumbPath += ` <span style="color: darkgrey;">/</span> <strong>${capitalizeFirstLetter(
-          pathParts[i]
-        )}</strong>`;
-      } else if (i === 0) {
-        breadcrumbPath += ` <a href="/${pathBefore}" style="text-decoration: none;">${capitalizeFirstLetter(
-          pathParts[i]
-        )}</a>`;
+        breadcrumbPath += ` ${
+          isFirstSegment ? "" : "<span style='color: darkgrey;'>/</span>"
+        } <strong>${capitalizeFirstLetter(pathParts[i])}</strong>`;
       } else {
-        if (pathBefore.startsWith("/dashboard/")) {
-          breadcrumbPath += ` <span style="color: darkgrey;">/</span> Dashboard`;
-          const afterDashboard = pathBefore.substring("/dashboard/".length);
-          breadcrumbPath += ` <a href="/dashboard/${afterDashboard}" style="text-decoration: none;">${capitalizeFirstLetter(
-            pathParts[i]
-          )}</a>`;
-        } else {
-          breadcrumbPath += ` <span style="color: darkgrey;">/</span> <a href="/${pathBefore}" style="text-decoration: none;">${capitalizeFirstLetter(
-            pathParts[i]
-          )}</a>`;
-        }
+        breadcrumbPath += isFirstSegment
+          ? ` <a href="/${pathBefore}" style="text-decoration: none;">${capitalizeFirstLetter(
+              pathParts[i]
+            )}</a>`
+          : ` <span style="color: darkgrey;">/</span> <a href="/${pathBefore}" style="text-decoration: none;">${capitalizeFirstLetter(
+              pathParts[i]
+            )}</a>`;
       }
     }
 
