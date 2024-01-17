88be0809ba553c8bfb6b28b718d27e94479f3c2a
Julian Hammer <jh@moinjulian.com>
2024-01-17 22:03:53 +0000

    Add badge descriptions and interactivity.

--- src/routes/[name]/+page.svelte
+++ src/routes/[name]/+page.svelte
@@ -22,6 +22,12 @@
     event.preventDefault();
     editing = false;
   }
+
+  let activeBadgeIndex: number | null;
+
+  function handleBadgeClick(index: number) {
+    activeBadgeIndex = index === activeBadgeIndex ? null : index;
+  }
 </script>
 
 <svelte:head>
@@ -54,12 +60,21 @@
     <h3>Highlights</h3>
     <div class="badges">
       {#if data.badges}
-        {#each data.badges as badge}
+        {#each data.badges as badge, index}
+          <!-- svelte-ignore a11y-click-events-have-key-events -->
+          <!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
           <p
             style="border: 1px solid {badge.color}; width: max-content; border-radius: 24px; padding: 3px 5px; color: {badge.color}; margin: 3px 5px; font-size: var(--tiny-font)"
+            on:click={() => handleBadgeClick(index)}
           >
             {badge.title.toUpperCase()}
+            {index}
           </p>
+          {#if activeBadgeIndex === index}
+            <div class="description">
+              <p>{badge.description}</p>
+            </div>
+          {/if}
         {/each}
       {/if}
     </div>
@@ -171,6 +186,18 @@
       margin-top: 10px;
       display: flex;
       flex-direction: row;
+
+      .description {
+        position: absolute;
+        margin-top: 30px;
+        background-color: var(--nav-color);
+        border: 1px solid var(--border-color);
+        width: 260px;
+        padding: 10px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      }
     }
   }
 </style>
