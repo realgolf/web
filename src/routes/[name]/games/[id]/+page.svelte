76f8c774b7326650c6cd1fb6d06c73a1b67562d0
Julian Hammer <jh@moinjulian.com>
2024-01-20 17:29:24 +0000

    Add page for a single game

---
+++ src/routes/[name]/games/[id]/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import FourWinningTable from "$lib/components/FourWinning_table.svelte";
+  
+  export let data;
+</script>
+
+<svelte:head>
+  <title>{data.game.name} from {data.param_name}</title>
+</svelte:head>
+
+<h1>
+  {data.game.name} saved on the {new Date(data.game.date).toLocaleDateString()}
+</h1>
+
+<p class="data">{data.game.data}</p>
+
+{#if data.game.teams.includes("4winning_")}
+     <FourWinningTable measurement_units="yards" data={data.game.data}></FourWinningTable>
+{/if}
