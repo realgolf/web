dd47c0e8478a42dd7c74a78e055ae3699cde78fe
Julian Hammer <jh@moinjulian.com>
2024-01-20 17:41:52 +0000

    Add 404 page for non-existent games

--- src/routes/[name]/games/[id]/+page.svelte
+++ src/routes/[name]/games/[id]/+page.svelte
@@ -1,19 +1,31 @@
 <script lang="ts">
   import FourWinningTable from "$lib/components/FourWinning_table.svelte";
-  
+
   export let data;
 </script>
 
 <svelte:head>
-  <title>{data.game.name} from {data.param_name}</title>
+  {#if data.game}
+    <title>{data.game.name} from {data.param_name}</title>
+  {:else}
+    <title>404- Game not found</title>
+  {/if}
 </svelte:head>
 
 <h1>
-  {data.game.name} saved on the {new Date(data.game.date).toLocaleDateString()}
+  {#if data.game}
+    {data.game.name} saved on the {new Date(
+      data.game.date
+    ).toLocaleDateString()}
+  {:else}
+    404 - Game not found for this User.
+  {/if}
 </h1>
 
-<p class="data">{data.game.data}</p>
+{#if data.game}
+  <p class="data">{data.game.data}</p>
 
-{#if data.game.teams.includes("4winning_")}
-     <FourWinningTable measurement_units="yards" data={data.game.data}></FourWinningTable>
+  {#if data.game.teams.includes("4winning_")}
+    <FourWinningTable measurement_units="yards" data={data.game.data} />
+  {/if}
 {/if}
