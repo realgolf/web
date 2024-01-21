7bdf44b3cd55b8ae6acfae14c1115aeeac8fbcfe
Julian Hammer <jh@moinjulian.com>
2024-01-21 15:38:14 +0000

    Add first unlocked function

--- src/lib/server/achievements.ts
+++ src/lib/server/achievements.ts
@@ -6,14 +6,15 @@ export async function check_achievement(user: User, gameId: string) {
   const total_games = user.total_games;
   const user_name = user.user.username;
 
-  check_bronze_requirements(total_games, achievements, user_name, gameId);
+  check_first_unlock(total_games, achievements, user_name, gameId);
+  // check_bronze_requirements(total_games, achievements, user_name, gameId);
 
   user.achievements = achievements;
 
   console.log(user);
 }
 
-function check_bronze_requirements(
+function check_first_unlock(
   total_games: number,
   achievements: Achievements[],
   user_name: string,
@@ -25,7 +26,7 @@ function check_bronze_requirements(
       description: `${user_name} played games that have been saved`,
       first_unlocked: new Date(),
       is_unlocked: {
-        bronze_unlocked: true,
+        bronze_unlocked: false,
         silver_unlocked: false,
         gold_unlocked: false,
         platinum_unlocked: false,
@@ -41,3 +42,38 @@ function check_bronze_requirements(
     return "";
   }
 }
+
+// function check_bronze_requirements(
+//   total_games: number,
+//   achievements: Achievements[],
+//   user_name: string,
+//   gameId: string
+// ) {
+//   if (total_games == 16) {
+//     achievements.push({
+//       title: "Game Master",
+//       description: `${user_name} played games that have been saved`,
+//       first_unlocked: new Date(),
+//       is_unlocked: {
+//         bronze_unlocked: true,
+//         silver_unlocked: false,
+//         gold_unlocked: false,
+//         platinum_unlocked: false,
+//       },
+//       history: {
+//         bronze_unlocked_date: new Date(),
+//         bronze_unlocked_game: gameId,
+//       },
+//     });
+
+//     return achievements;
+//   } else {
+//     return "";
+//   }
+// }
+
+// function check_silver_requirements(total_games: number, achievements: Achievements, user_name: string, gameId: string) {
+//   if(total_games == 128) {
+
+//   }
+// }
