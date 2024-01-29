9021376afc63ba2650df0ab9929df6674610007b
Julian Hammer <jh@moinjulian.com>
2024-01-29 19:25:23 +0000

    Add a score subsite

---
+++ src/routes/dashboard/score/+page.server.ts
@@ -0,0 +1,13 @@
+import { User_Model } from "$lib/server/models";
+import type { PageServerLoad } from "./$types";
+
+export const load: PageServerLoad = async (event): Promise<unknown> => {
+  const email = event.cookies.get("email");
+
+  const user = await User_Model.findOne({ "user.email": email });
+
+  const name = user?.user?.name as string;
+  const handicap = user?.user?.handicap as number;
+
+  return { name, handicap };
+};
---
+++ src/routes/dashboard/score/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  export let data: any;
+</script>
+
+<svelte:head>
+  <title>Score Tracker</title>
+</svelte:head>
+
+<h1>Score Tracker</h1>
+
+<main>
+  <div class="heading">
+    <p>
+      Welcome to our Real Golf Score Tracker {data.name}. <br /> Your current
+      Handicap is: <b>{data.handicap}</b>
+    </p>
+
+    <a href="/play">Play</a>
+  </div>
+</main>
