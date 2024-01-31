5892fae42238b06e1ef8a28de43dc1c101b6cc1a
Julian Hammer <jh@moinjulian.com>
2024-01-31 19:25:36 +0000

    Enable project to work with node

--- svelte.config.js
+++ svelte.config.js
@@ -13,8 +13,11 @@
 // export default config;
 
 import adapter from "@sveltejs/adapter-node";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
+/** @type {import('@sveltejs/kit').Config} */
 export default {
+  preprocess: vitePreprocess(),
   kit: {
     adapter: adapter(),
   },
