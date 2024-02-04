421cd406df0640206b357900c7427b287ac21e5f
Julian Hammer <jh@moinjulian.com>
2024-02-04 22:37:43 +0000

    Add list of Social Media Platforms

---
+++ src/lib/scripts/social_links.ts
@@ -0,0 +1,48 @@
+import {
+  faFacebook,
+  faGithub,
+  faInstagram,
+  faLinkedin,
+  faPinterest,
+  faYoutube,
+} from "@fortawesome/free-brands-svg-icons";
+import { faChess, faChessKnight, faX } from "@fortawesome/free-solid-svg-icons";
+
+export const social_links = [
+  {
+    logo: faGithub,
+    domain: "https://github.com/*",
+  },
+  {
+    logo: faYoutube,
+    domain: "https://youtube.com/*",
+  },
+  {
+    logo: faInstagram,
+    domain: "https://instagram.com/*",
+  },
+  {
+    logo: faFacebook,
+    domain: "https://facebook.com/*",
+  },
+  {
+    logo: faPinterest,
+    domain: "https://pinterest.com/*",
+  },
+  {
+    logo: faLinkedin,
+    domain: "https://linkedin.com/*",
+  },
+  {
+    logo: faChess,
+    domain: "https://www.chess.com/members/*",
+  },
+  {
+    logo: faChessKnight,
+    domain: "https://lichess.org/@/*",
+  },
+  {
+    logo: faX,
+    domain: "https://x.com/*",
+  },
+];
