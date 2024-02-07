fb7e44580deab1695b172973612f3e53de9c7a79
Julian Hammer <jh@moinjulian.com>
2024-02-07 19:35:04 +0000

    Revert "Merge branch '9-feature-let-user-choose-size-for-game-and-color-for-player'"
    This reverts commit 6a7eff8bd70a1de2df2270092aa5169c3899bc73, reversing
    changes made to 1583cf0f4d6a7d94d2e4910a8a41e18083c8360c.

---
+++ src/routes/dashboard/games/4Winning/Four-Players/+page.server.ts
@@ -0,0 +1,87 @@
+import { check_achievement } from "$lib/server/achievements";
+import type { User } from "$lib/server/interface";
+import { User_Model } from "$lib/server/models"; // Assuming you have a User model
+import { v4 as uuidv4 } from "uuid";
+import type { Actions, PageServerLoad } from "./$types";
+
+export const load: PageServerLoad = async (event) => {
+  const email = event.cookies.get("email");
+
+  const user = await User_Model.findOne({ "user.email": email });
+
+  if (!user) {
+    return { status: 400, error: new Error("User could not be found") };
+  } else {
+    const measurement_unit = user.user?.measurement_units;
+    return { measurement_unit };
+  }
+};
+
+export const actions: Actions = {
+  default: async (event) => {
+    const data = await event.request.formData();
+    const raw_team_data = data.get("team_data") as string;
+
+    let team_data;
+    try {
+      team_data = JSON.parse(raw_team_data);
+    } catch (jsonParseError) {
+      console.error("Error parsing team_data:", jsonParseError);
+      return {
+        status: 400,
+        body: JSON.stringify({ error: "Invalid team_data JSON" }),
+      };
+    }
+    const email = event.cookies.get("email");
+
+    try {
+      // Find the user
+      const user = await User_Model.findOne({ "user.email": email });
+
+      if (!user) {
+        return {
+          status: 404,
+          body: JSON.stringify({ error: "User not found" }),
+        };
+      }
+
+      // Generate a unique ID for the game
+      const gameId = uuidv4();
+
+      const today = new Date();
+      const year = today.getFullYear();
+      const month = (today.getMonth() + 1).toString().padStart(2, "0");
+      const day = today.getDate().toString().padStart(2, "0");
+      const formattedDate = `${year}-${month}-${day}`;
+
+      // Add the game to the user
+      if (user.games) {
+        user.games.push({
+          id: gameId,
+          name: "4 Winning 4 Teams",
+          teams: "4winning_4_teams",
+          date: formattedDate,
+          data: JSON.stringify(team_data),
+        });
+
+        user.total_games += 1;
+        const user_copy = user as User;
+        check_achievement(user_copy, gameId);
+      }
+
+      // Save the user with the new game
+      await user.save();
+
+      return {
+        status: 200,
+        body: JSON.stringify({ message: "Game saved successfully" }),
+      };
+    } catch (error) {
+      console.error(error);
+      return {
+        status: 500,
+        body: JSON.stringify({ error: "Error saving game" }),
+      };
+    }
+  },
+};
