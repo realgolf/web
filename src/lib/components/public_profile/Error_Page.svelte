4ea34d1a80c92506a2ff032f2d50b12fb527d23d
Julian Hammer <jh@moinjulian.com>
2024-02-07 19:59:49 +0000

    Add Component for Error Page

---
+++ src/lib/components/public_profile/Error_Page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	export let path: string;
+</script>
+
+<div class="error-container">
+	<h1 class="error">404 - User/Page Not Found!</h1>
+	<p>
+		We couldn't find the user or page for the path: <strong>{path}</strong>
+	</p>
+	<p>Please check the path for any errors, ensure it is correctly spelled, and try again.</p>
+	<p>Go back to <a href="/">home</a>.</p>
+</div>
