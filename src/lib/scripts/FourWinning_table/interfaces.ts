d85cf6b8ba70caee740926991688cf2dc19478d5
Julian Hammer <jh@moinjulian.com>
2024-02-08 09:27:08 +0000

    Rename interfaces.ts to types.ts

--- src/lib/scripts/FourWinning_table/interfaces.ts
+++
@@ -1,9 +0,0 @@
-export interface Row {
-  side: string;
-  data: { id: string; value: string; color: string }[];
-}
-
-export interface GameData {
-  color: string;
-  data: string[];
-}
---
+++ src/lib/scripts/FourWinning_table/types.ts
@@ -0,0 +1,9 @@
+export type Row = {
+	side: string;
+	data: { id: string; value: string; color: string }[];
+};
+
+export type GameData = {
+	color: string;
+	data: string[];
+};
