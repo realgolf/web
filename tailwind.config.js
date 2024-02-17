7333925f2593742ed5f967e726f6e2162f2ae03a
Julian Hammer <jh@moinjulian.com>
2024-02-17 19:16:53 +0100

    Update tailwinf and postcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ["./src/**/*.{html,js,svelte,ts}"],
+	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
 		extend: {
 			colors: {
-				error: "#ffff00",
-				light: "#bbb",
-				dark: "#131416",
-			},
-		},
+				error: '#ffff00',
+				light: '#bbb',
+				dark: '#131416'
+			}
+		}
 	},
-	plugins: [],
+	plugins: []
 };
