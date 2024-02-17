a77def38d535aa770fa0e98ee9e135e0d6cad879
Julian Hammer <jh@moinjulian.com>
2024-02-17 19:02:30 +0100

    Add Construction Info at Course Tracker Page

--- src/routes/dashboard/courses/+page.svelte
+++ src/routes/dashboard/courses/+page.svelte
@@ -10,7 +10,7 @@
 
 <h1>Score Tracker</h1>
 
-<main>
+<!-- <main>
 	<div class="heading">
 		<p>
 			Welcome to our Real Golf Score Tracker {data.name}. <br /> Your current Handicap is:
@@ -19,4 +19,10 @@
 
 		<a href="/dashboard/courses/play">Play</a>
 	</div>
+</main> -->
+
+<main>
+	<h4>Welcome to the Real Golf Games Score Tracker {data.name}</h4>
+	<p class="error">This site is currently under development please come back later. 🏗️🚧</p>
+	<a href="/dashboard"><b>Back to your Dashboard</b></a>
 </main>
