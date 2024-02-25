6c2f3adfc4a374999343a6152288ae8460c66020
Julian Hammer <jh@moinjulian.com>
2024-02-25 20:55:41 +0000

    Refactor search_by_term function to make search case-insensitive

--- src/lib/scripts/Nav/search_by_term.ts
+++ src/lib/scripts/Nav/search_by_term.ts
@@ -3,6 +3,9 @@ import type { User } from '$lib/server/user/types';
 export function search_by_term(all_users: User[], search_term: string) {
 	// Filter all_users based on search_term
 	return all_users.filter((user) => {
-		return user.user.username.includes(search_term);
+		// lowercase both strings to make search case-insensitive
+		const lowerCaseSearchTerm = search_term.toLowerCase();
+		const lowerCaseUsername = user.user.username.toLowerCase();
+		return lowerCaseUsername.includes(lowerCaseSearchTerm);
 	});
 }
