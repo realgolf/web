9f7a24bdbc62928e9461e672a01f77b26df9795f
Julian Hammer <jh@moinjulian.com>
2024-03-05 17:19:41 +0000

    remove console logs

--- src/lib/scripts/Public/edit_status.ts
+++ src/lib/scripts/Public/edit_status.ts
@@ -4,8 +4,6 @@ export async function editStatus(email: string, status_text: string, busy: boole
 	const user = await User_Model?.findOne({ 'user.email': email });
 
 	if (user?.user?.status) {
-		console.log('status_text', status_text);
-		console.log('busy', busy);
 		user.user.status.text = status_text;
 		user.user.status.busy = busy;
 		await user.save();
