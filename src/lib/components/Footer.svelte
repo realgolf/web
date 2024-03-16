aab135a696380769c2e31cc5dc8defffee09c891
Julian Hammer <jh@moinjulian.com>
2024-03-16 18:47:13 +0000

    Update Footer component with new icons and links

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,6 +1,11 @@
 <script>
 	import { faInstagram, faXTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons';
-	import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+	import {
+		faBook,
+		faBookBookmark,
+		faBookOpenReader,
+		faEnvelope
+	} from '@fortawesome/free-solid-svg-icons';
 	import Fa from 'svelte-fa';
 </script>
 
@@ -8,14 +13,14 @@
 	<div class="container">
 		<div class="row">
 			<div class="same-line">
-				<div class="col-md-6">
+				<div class="col-md-6 first-row">
 					<h3>Contact Us</h3>
 					<p>
 						<Fa icon={faEnvelope} /> Email:
 						<a href="mailto:info@realgolf.games">info@realgolf.games</a>
 					</p>
 				</div>
-				<div class="col-md-6">
+				<div class="col-md-6 first-row">
 					<h3>Follow Us</h3>
 					<p>
 						<Fa icon={faXTwitter} /> Twitter:
@@ -29,6 +34,12 @@
 						<Fa icon={faYoutube} /> YouTube:
 						<a href="https://www.youtube.com/@realgolfgames">RealGolfGames</a>
 					</p>
+				</div>
+				<div class="col-md-6 first-row">
+					<h3>Explore More</h3>
+					<p><Fa icon={faBookOpenReader} /> <a href="https://blog.realgolf.games">Blog</a></p>
+					<p><Fa icon={faBook} /> <a href="https://docs.realgolf.games">Documentation</a></p>
+					<p><Fa icon={faBookBookmark} /> <a href="https://rules.realgolf.games">Golfrules</a></p>
 				</div>
 			</div>
 			<div class="col-md-6">
@@ -51,11 +62,20 @@
 		.row {
 			width: 50vw;
 
+			.first-row:not(:first-child) {
+				margin: 0 2rem;
+			}
+
 			.same-line {
-				display: flex;
-				flex-direction: row;
-				align-items: top;
-				justify-content: space-between;
+				display: flex !important;
+				flex-wrap: wrap !important;
+
+				@media only screen and (max-width: 767px) {
+					margin-right: -16px;
+					margin-left: -16px;
+					display: flex;
+					flex-direction: column;
+				}
 			}
 		}
 	}
