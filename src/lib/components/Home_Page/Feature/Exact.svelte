0f5312c32b09e327f3127c83906275bf0c823ede
Julian Hammer <jh@moinjulian.com>
2024-03-28 14:17:21 +0100

    Refactor Exact game description and add alt text to image

--- src/lib/components/Home_Page/Feature/Exact.svelte
+++ src/lib/components/Home_Page/Feature/Exact.svelte
@@ -1,12 +1,16 @@
 <div class="feature">
 	<h3>Exact</h3>
 	<p>
-		Exact is a game which goal it is to play exact as possible to a few numbers, for example every
-		tens give 3 points and all numbers with twice the same number are giving 3 Points, 100 meters
-		gives 5 points, everything above 100 and below 5 gives -1 Points.
+		Exact is a game where the objective is to hit 100 or below while scoring the most points.
+		Players earn points based on the following criteria: Achieving exactly 100 meters awards 5
+		points, hitting multiples of ten earns 3 points, numbers with repeating digits score 2 points.
+		Additionally, hitting the same row doubles the points earned. However, any other number
+		exceeding 100 or falling below 5 results in a deduction of 1 point. Every other number between 5
+		and 100 scores 1 point. The challenge lies in balancing accuracy with maximizing points to
+		achieve the highest score.
 	</p>
 
-	<img src="/home_page/Exact.png" alt="All teams are tie in Exact">
+	<img src="/home_page/Exact.png" alt="All teams are tie in Exact" />
 </div>
 
 <style lang="scss">
