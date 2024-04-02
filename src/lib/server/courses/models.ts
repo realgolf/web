6bc4c5f63fa642955206ac8f2fb0a8843ade6f12
Julian Hammer <jh@moinjulian.com>
2024-04-02 20:10:08 +0100

    Update course model

--- src/lib/server/courses/models.ts
+++ src/lib/server/courses/models.ts
@@ -8,9 +8,9 @@ const Course_Schema = new mongoose.Schema({
 	id: { type: String, require: true, unique: true },
 	name: { type: String, require: true, unique: true },
 	location: { type: String, require: true },
-	par: { type: Number, require: true },
-	out_par: { type: Number, require: true },
 	in_par: { type: Number, require: true },
+	out_par: { type: Number, require: true },
+	total_par: { type: Number, require: true },
 	tees: [
 		{
 			color: { type: String, require: true },
@@ -22,8 +22,8 @@ const Course_Schema = new mongoose.Schema({
 					hcp: { type: Number, require: true }
 				}
 			],
-			out_distance: { type: Number, require: true },
 			in_distance: { type: Number, require: true },
+			out_distance: { type: Number, require: true },
 			total_distance: { type: Number, require: true }
 		}
 	],
