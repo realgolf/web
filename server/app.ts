287ce7c0ab7dff82131cf125ebd11f70f908677d
Julian Hammer <jh@moinjulian.com>
2024-04-10 15:10:00 +0200

    Refactor socket handling in server

--- server/app.ts
+++ server/app.ts
@@ -1,13 +1,13 @@
 import dotenv from 'dotenv';
 import { connect_to_db } from './mongodb.js';
 import { generate_server } from './server.js';
-import { attach_sockets } from './sockets.js';
+import { handle_sockets } from './sockets.js';
 import { purge_old_users } from './user.js';
 
 async function init() {
 	dotenv.config();
 	const server = generate_server();
-	attach_sockets(server);
+	handle_sockets(server);
 	await connect_to_db();
 	await purge_old_users();
 }
