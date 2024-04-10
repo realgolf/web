fbda1c1f3a6c678c463f653c625fbf461e3354d7
Julian Hammer <jh@moinjulian.com>
2024-04-10 14:00:54 +0200

    Add JSDoc Comments for shared

--- src/lib/shared/time.ts
+++ src/lib/shared/time.ts
@@ -1,16 +1,38 @@
-//Add Date and time
+/**
+ * Get the current date and time
+ */
 export const today = new Date();
 
-// Get the current time
+/**
+ * Get the current time
+ * Hours, minutes, and seconds
+ */
 const hours = today.getHours();
 const minutes = today.getMinutes();
 const seconds = today.getSeconds();
 
-// Format the time
+/**
+ * Format the time
+ */
 export const formattedTime =
 	hours + ':' + (minutes < 10 ? '0' : '') + minutes + ':' + (seconds < 10 ? '0' : '') + seconds;
 
+/**
+ * Get the full Years
+ */
 const year = today.getFullYear();
+
+/**
+ * Gett the full Month
+ */
 const month = (today.getMonth() + 1).toString().padStart(2, '0');
+
+/**
+ * Get the full Day
+ */
 const day = today.getDate().toString().padStart(2, '0');
+
+/**
+ * Format the date
+ */
 export const formattedDate = `${year}-${month}-${day}`;
