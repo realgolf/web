8de1b58600d69861e390bdf6b7033709c2eb853a
Julian Hammer <jh@moinjulian.com>
2024-04-10 13:03:46 +0200

    Add JSDoc Comments

--- src/lib/scripts/Archive/types.ts
+++ src/lib/scripts/Archive/types.ts
@@ -1,16 +1,14 @@
 /**
  * Represents an array of filtered games.
- * It can contain either an array of `games` objects or an array of any other type.
+ * It contains an array of games.
  */
-// eslint-disable-next-line
-export type filteredGames = games[] | any[];
+export type filteredGames = games[];
 
 /**
  * Represents the data object.
  * It contains properties such as authentication token, email, an array of `games` objects,
  * measurement units, name, and theme.
  */
-
 export type Data = {
 	auth: string;
 	email: string;
