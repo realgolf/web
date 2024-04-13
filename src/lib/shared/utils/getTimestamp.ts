f349f459ea5ecddaf67c2df88f43cca8cc2a8599
Julian Hammer <jh@moinjulian.com>
2024-04-13 10:56:16 +0200

    Refactor get_timestamp function to use padStart for formatting

--- src/lib/shared/utils/getTimestamp.ts
+++ src/lib/shared/utils/getTimestamp.ts
@@ -6,15 +6,10 @@ export function get_timestamp() {
 	const date = new Date();
 
 	// Get hours, minutes, and seconds from the date object
-	const hours = date.getHours();
-	const minutes = date.getMinutes();
-	const seconds = date.getSeconds();
-
-	// Add leading zeros if the value is less than 10
-	const formattedHours = hours < 10 ? `0${hours}` : hours;
-	const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
-	const formattedSeconds = seconds < 10 ? `0${seconds}` : seconds;
+	const hours = String(date.getHours()).padStart(2, '0');
+	const minutes = String(date.getMinutes()).padStart(2, '0');
+	const seconds = String(date.getSeconds()).padStart(2, '0');
 
 	// Concatenate formatted time components with colons
-	return `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
+	return `${hours}:${minutes}:${seconds}`;
 }
