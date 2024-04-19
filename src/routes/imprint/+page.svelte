56aee03af536b147238dcea8d5c0b35473d99874
Julian Hammer <jh@moinjulian.com>
2024-04-19 09:34:33 +0100

    Modify Informtaion on Imprint Page for Crowdin

--- src/routes/imprint/+page.svelte
+++ src/routes/imprint/+page.svelte
@@ -1,75 +1,65 @@
+<script lang="ts">
+	import { _, isLoading } from 'svelte-i18n';
+</script>
+
 <svelte:head>
-	<title>Real Golf - Imprint</title>
+	<title>Real Golf - {$_('imprint_heading')}</title>
 </svelte:head>
 
-<h1>Imprint</h1>
+{#if $isLoading}
+	<p>Loading...</p>
+{:else}
+	<h1>{$_('imprint_heading')}</h1>
 
-<h2>According to § 5 TMG (German Telemedia Law)</h2>
+	<h2>{$_("responsible_according_tmg")}</h2>
 
-<p translate="no">RealGolf.Games</p>
-<p>c/o Julian Hammer</p>
-<p>Katrepeler Landstraße 59a</p>
-<p>28357 Bremen</p>
+	<p translate="no">RealGolf.Games</p>
+	<p>c/o Julian Hammer</p>
+	<p>Katrepeler Landstraße 59a</p>
+	<p>28357 Bremen</p>
 
-<h2>Contact</h2>
+	<h2>{$_("contact")}</h2>
 
-<p>E-Mail: jh@moinjulian.com</p>
+	<p>E-Mail: jh@moinjulian.com</p>
 
-<h2>Responsible for the content according to § 55 Abs. 2 RStV</h2>
+	<h2>{$_("responsible_according_rstv")}</h2>
 
-<p translate="no">RealGolf.Games</p>
-<p>c/o Julian Hammer</p>
-<p>Katrepeler Landstraße 59a</p>
-<p>28357 Bremen</p>
+	<p translate="no">RealGolf.Games</p>
+	<p>c/o Julian Hammer</p>
+	<p>Katrepeler Landstraße 59a</p>
+	<p>28357 Bremen</p>
 
-<h2>Liability for content</h2>
-<p>
-	As a service provider, we are responsible for our own content on these pages according to Section
-	7, Paragraph 1 of the German Telemedia Act (TMG). According to §§ 8 to 10 TMG, however, we as a
-	service provider are not obliged to monitor transmitted or stored third-party information or to
-	investigate circumstances that indicate illegal activity.
-</p>
+	<h2>{$_("liability_for_content")}</h2>
+	<p>
+		{$_("content_for_liability_for_content")}
+	</p>
 
-<p>
-	Obligations to remove or block the use of information according to general laws remain unaffected.
-	However, liability in this regard is only possible from the point in time at which knowledge of a
-	specific infringement of the law is known. As soon as we become aware of any violations of the
-	law, we will remove this content immediately.
-</p>
+	<p>
+		{$_("content_for_liability_for_content_2")}
+	</p>
 
-<h2>Liability for links</h2>
+	<h2>{$_("liability_for_links")}</h2>
 
-<p>
-	Our offer contains links to external websites over which we have no influence. Therefore we cannot
-	assume any liability for this external content. The respective provider or operator of the pages
-	is always responsible for the content of the linked pages. The linked pages were checked for
-	possible legal violations at the time of linking. Illegal content was not recognizable at the time
-	of linking.
-</p>
+	<p>
+		{$_("content_for_liability_for_links")}
+	</p>
 
-<p>
-	However, a permanent control of the content of the linked pages is not reasonable without concrete
-	evidence of an infringement. As soon as we become aware of legal violations, we will remove such
-	links immediately.
-</p>
+	<p>
+		{$_("content_for_liability_for_links_2")}
+	</p>
 
-<h2>Copyright</h2>
+	<h2>{$_("copyright_imprint")}</h2>
 
-<p>
-	The content and works on these pages created by the site operators are subject to German copyright
-	law. The duplication, editing, distribution and any kind of exploitation outside the limits of
-	copyright require the written consent of the respective author or creator. Downloads and copies of
-	this site are only permitted for private, non-commercial use.
-</p>
+	<p>
+		{$_("content_for_copyright_imprint")}
+	</p>
 
-<p>
-	Insofar as the content on this site was not created by the operator, the copyrights of third
-	parties are observed. In particular contents of third parties are marked as such. Should you
-	nevertheless become aware of a copyright infringement, we ask that you inform us accordingly. As
-	soon as we become aware of legal violations, we will remove such content immediately.
-</p>
+	<p>
+		{$_("content_for_copyright_imprint_2")}
+	</p>
 
-<p>Copyright © 2024 RealGolf.Games</p>
-<p>All Rights Reserved</p>
+	<p>Copyright © 2024 RealGolf.Games</p>
+	<p>{$_("license")}</p>
 
-<p>Germany 04.07.23</p>
+	<p>{$_("country")} 04.07.23</p>
+{/if}
