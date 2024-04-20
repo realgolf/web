bd649bc8e77f332fa4e7073f93c76db1b7a0dfe8
Julian Hammer <jh@moinjulian.com>
2024-04-20 20:37:31 +0100

    Display the full name of a language as the choose option

--- src/lib/components/Nav/LanguageSwitcher.svelte
+++ src/lib/components/Nav/LanguageSwitcher.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { isoLangs } from '$lib/i18n/ISO_639-1';
+	import { capitalizeFirstLetter } from '$lib/shared/utils/capitalizeFirstLetter';
 	import { faGlobe } from '@fortawesome/free-solid-svg-icons';
 	import Fa from 'svelte-fa';
 	import { locale, locales } from 'svelte-i18n';
@@ -14,6 +16,16 @@
 	function closeLanguageSelector() {
 		isLanguageSwitcherOpen = false;
 	}
+
+	// Function to handle changing the locale
+	function changeLocale(newLocale: string) {
+		locale.set(newLocale);
+	}
+
+    // Function to get the Native name of the language
+    function getNativeLang(lang: string) {
+        return capitalizeFirstLetter((isoLangs as Record<string, { name: string; nativeName: string }>)[lang].nativeName.split(',')[0].trim().replace(/\s/g, ''));
+    }
 
 	// onMount(() => {
 	// 	window.addEventListener('keypress', (e) => {
@@ -51,15 +63,22 @@
 	<div class="blurry-background" />
 	<div class="language-switcher-content">
 		<button class="close-button" on:click={closeLanguageSelector}>x</button>
-		{$locale}
-		<select bind:value={$locale}>
-			{#each $locales as locale}
-				<option value={locale}>{locale}</option>
-			{/each}
-		</select>
+		{#each $locales as lang}
+			<button id={`language-button-${lang}`} class="language-selector-btn" on:click={() => changeLocale(lang)}>{getNativeLang(lang)}</button>
+		{/each}
 	</div>
 {/if}
 
 <style lang="scss">
 	@import '$lib/scss/Nav/LanguageSwitcher.scss';
+
+    .language-selector-btn {
+        background-color: transparent;
+        border: 1px solid var(--border-color) !important;
+        color: inherit;
+        cursor: pointer;
+        font-size: 1rem;
+        margin: 0.5rem;
+        padding: 0.5rem;
+    }
 </style>
