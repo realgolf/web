80afa19b2343120ef9136484275a8ff6db27508d
Julian Hammer <jh@moinjulian.com>
2024-04-21 12:58:15 +0100

    Add Info to Crowdin

--- src/routes/dashboard/info/+page.svelte
+++ src/routes/dashboard/info/+page.svelte
@@ -1,18 +1,21 @@
+<script lang="ts">
+	import { _, isLoading } from 'svelte-i18n';
+</script>
+
 <svelte:head>
-	<title>Real Golf - Info</title>
+	<title>Real Golf - {$_('info')}</title>
 </svelte:head>
 
-<h1>Info</h1>
+{#if $isLoading}
+	<p>Loading...</p>
+{:else}
+	<h1>{$_('info')}</h1>
 
-<p>
-	The games are for golfers who want to train a different way, you only need a kind of trackman or a
-	simulator to determine the exact length. After you have determined the length with your trackman
-	(or the like). You can click the Field with number you played or enter the number in the input
-	Field.
-	<br />
-	<b>Have fun playing</b>
-</p>
+	<p>
+		{@html $_('info_text')}
+	</p>
 
-<p>
-	A list of some things that can be used can be found <a href="/dashboard/tools">here</a>.
-</p>
+	<p>
+		{@html $_("link_to_tools")}
+	</p>
+{/if}
