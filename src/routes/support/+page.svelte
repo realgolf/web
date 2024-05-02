622713423786e46610fdf8493d87b649cc5fb2a1
Julian Hammer <jh@moinjulian.com>
2024-05-02 12:59:23 +0100

    Add Support Form

---
+++ src/routes/support/+page.svelte
@@ -0,0 +1,34 @@
+<svelte:head>
+	<title>Real Golf - Support</title>
+</svelte:head>
+
+<h1>Support</h1>
+
+<form action="?/support" method="POST" autocomplete="off">
+	<label for="name">Name</label>
+	<input type="text" id="name" name="name" />
+	<label for="email">Your Email</label>
+	<input type="text" id="email" name="email" />
+	<label for="subject">Subject</label>
+	<select name="subject" id="subject">
+		<option value="other">Other</option>
+		<option value="account">Account</option>
+		<!-- <option value="payment">Payment</option> -->
+		<option value="technical">Technical</option>
+		<option value="bug">Bug</option>
+		<option value="feature">Feature Request</option>
+	</select>
+	<label for="message">Message</label>
+	<textarea name="message" id="message" cols="30" rows="10" />
+	<br />
+	<button type="submit">Submit</button>
+</form>
+
+<style lang="scss">
+	textarea {
+		background-color: var(--background-color);
+		color: var(--font-color);
+		font-size: var(--medium-font);
+		width: 100%;
+	}
+</style>
