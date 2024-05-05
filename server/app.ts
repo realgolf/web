b90a2f8a740c444f2625eee75b6e3f5cf638fc39
Julian Hammer <jh@moinjulian.com>
2024-05-05 13:22:17 +0100

    Revert changes

--- server/app.ts
+++ server/app.ts
@@ -1,16 +1,11 @@
 import dotenv from 'dotenv';
-import { connect_to_db, disconnect_from_db } from './db.js';
 import { generate_server } from './server.js';
 import { handle_sockets } from './sockets.js';
-import { deleteInactiveUsers } from './users.js';
 
 async function init() {
 	dotenv.config();
 	const server = generate_server();
 	handle_sockets(server);
-	await connect_to_db();
-	await deleteInactiveUsers();
-	await disconnect_from_db();
 }
 
 init();
